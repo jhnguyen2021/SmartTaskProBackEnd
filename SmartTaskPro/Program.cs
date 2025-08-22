fb139c1193f3700b11a81e600d6569d8c2c03223
Jason Nguyen <jhnguyen2021@gmailcom>
2025-08-21 22:36:25 -0400

    Add JWT token generation logic  Build Login and Register endpoints  Secure routes with `[Authorize]` and roles Test login flow (Postman or Swagger)

--- SmartTaskPro/Program.cs
+++ SmartTaskPro/Program.cs
@@ -1,6 +1,7 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using SmartTaskPro.Data;
@@ -8,6 +9,7 @@ using SmartTaskPro.Mappings;
 using SmartTaskPro.Services;
 using SmartTaskPro.Services.Interfaces;
 using System.Text;
+using System.Text.Json.Serialization;
 
 
 
@@ -16,6 +18,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Identity (GUID keys)
+//builder.Services.AddIdentity<User, IdentityRole<Guid>>(opt =>
+//{
+//    opt.Password.RequiredLength = 6;
+//    opt.Password.RequireNonAlphanumeric = false;
+//})
+//.AddEntityFrameworkStores<AppDbContext>()
+//.AddDefaultTokenProviders();
+
+
 
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
 var jwt = builder.Configuration.GetSection("Jwt").Get<JwtSettings>()!;
@@ -41,6 +55,7 @@ builder.Services.AddAuthentication(opt =>
 builder.Services.AddAutoMapper(cfg => cfg.AddProfile<AppProfile>());
 //builder.Services.AddAutoMapper(typeof(Program).Assembly);
 //builder.Services.AddAutoMapper(typeof(MappingProfile));
+
 builder.Services.AddFluentValidationAutoValidation();
 
 builder.Services.AddValidatorsFromAssemblyContaining<SmartTaskPro.Validation.ProjectCreateValidator>();
@@ -50,7 +65,16 @@ builder.Services.AddScoped<IProjectService, ProjectService>();
 builder.Services.AddScoped<ITaskService, TaskService>();
 
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(opt =>
+    {
+        opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        opt.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -71,6 +95,26 @@ builder.Services.AddScoped<ITaskService, TaskService>();
 //builder.Services.AddScoped<JwtHelper>();
 //builder.Services.AddScoped<PasswordHasher>();
 
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var errors = context.ModelState
+            .Where(kvp => kvp.Value?.Errors.Count > 0)
+            .ToDictionary(
+                kvp => kvp.Key,
+                kvp => kvp.Value!.Errors.Select(e => e.ErrorMessage).ToArray());
+
+        var problem = new ValidationProblemDetails(errors)
+        {
+            Status = StatusCodes.Status400BadRequest,
+            Title = "Validation failed."
+        };
+        return new BadRequestObjectResult(problem);
+    };
+});
+
+builder.Services.AddRouting(o => o.LowercaseUrls = true);
 
 
 var app = builder.Build();
@@ -87,5 +131,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+//using (var scope = app.Services.CreateScope())
+//{
+//    await IdentitySeeder.SeedAsync(scope.ServiceProvider);
+//}
+
 
 app.Run();
